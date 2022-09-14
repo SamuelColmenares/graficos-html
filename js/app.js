95e8139a09c64d5982a15726d7f104db3cbab071
Samuel Colmenares <locoucla@hotmail.com>
2022-09-13 20:24:40 -0500

    canvas

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function pintar() {
+    const canvas = document.getElementById('lienzo');
+
+    if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
+        console.log('ctx :>> ', ctx);
+        /*ctx.fillStyle = 'rgb(200,0,0)';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0,0,200,0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+
+        ctx.beginPath();
+        ctx.moveTo(75, 50);
+        ctx.lineTo(100, 75);
+        ctx.lineTo(100, 25);
+        ctx.fill();*/
+
+
+    }
+}
+
+pintar();
